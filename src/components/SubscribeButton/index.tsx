bf65eaee22185cd3e173f930a74c8759ae1f24ae
Renato Theodoro <renatotheojunior@gmail.com>
2022-02-03 18:06:07 -0300

    Trabalhando com stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripeJs";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonPrice {
@@ -7,13 +9,21 @@ interface SubscribeButtonPrice {
 
 export function SubscribeButton({priceId} : SubscribeButtonPrice ){
     const {data:session} = useSession();
-    function handleSubscribeButton(){
+    async function handleSubscribeButton(){
         if(!session){
             signIn('github');
             return;
         }
 
         //Criação da checkout session
+        try{
+            const response = await api.post('/subscribe')
+            const {sessionId} = response.data;
+            const stripe = getStripeJs();
+            (await stripe).redirectToCheckout({sessionId: sessionId})
+        }catch(err){
+            alert(err.message);
+        }
         
 
     }
