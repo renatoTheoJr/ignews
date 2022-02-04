a05c1dcc506f872d9f45efcc6fbf95e72536f01f
Renato Theodoro <renatotheojunior@gmail.com>
2022-02-04 20:01:18 -0300

    Webhooks stripe

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,54 @@
+import { fauna } from "../../../services/fauna";
+import {query as q} from 'faunadb'
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscrition(
+    subscriptionId: string,
+    customerId: string,
+    createAction = false
+) {
+    //Buscar o usuario no fauna - Stripe CustomerId
+    //Salvar os dados da subscrition no Fauna
+    const userRef = await fauna.query(
+        q.Select(
+            "ref",
+            q.Get(
+                q.Match(
+                    q.Index('user_by_stripe_customer_id'),
+                    customerId
+                )
+            )
+        )
+    )
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+    const subscriptionData = {
+        id: subscription.id,
+        user_id: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id
+    }
+    if(createAction){
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                {data:subscriptionData}
+            )
+        )
+    }else{
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId
+                        )
+                    )
+                ),
+                {data: subscriptionData}
+            )
+            
+        )
+    }
+}
