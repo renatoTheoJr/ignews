a43afe8df288bdf97e41cd365fefde88808b705b
Renato Theodoro <renatotheojunior@gmail.com>
2022-02-13 23:11:16 -0300

    Listando Posts e navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
 export function Header(){
@@ -7,8 +8,12 @@ export function Header(){
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <Link href='/'>
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href='/posts' prefetch>
+                        <a>Posts</a>
+                    </Link>
                 </nav>
                 <SignInButton />
             </div>    
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,7 +4,21 @@ import Head from 'next/head';
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 import  Prismic  from '@prismicio/client';
-export default function Posts(){
+import {RichText} from 'prismic-dom'
+
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updateAt: string;
+
+}
+
+interface PostsProps {
+  posts : Post[]
+}
+
+export default function Posts( { posts } : PostsProps){
     return (
     <>
     <Head>
@@ -13,24 +27,18 @@ export default function Posts(){
     </Head>
     <main className={styles.container}>
         <div className={styles.posts}>
-            <a href='#'>
-                <time>13 de Fevereiro de 2022</time>
-                <strong>Esse é o titulo da noticias</strong>
-                <p>Esse paragráfo sera o conteúdo visível para o consumidor final</p>
-
-            </a>
-            <a>
-                <time>13 de Fevereiro de 2022</time>
-                <strong>Esse é o titulo da noticias</strong>
-                <p>Esse paragráfo sera o conteúdo visível para o consumidor final</p>
-
-            </a>
-            <a>
-                <time>13 de Fevereiro de 2022</time>
-                <strong>Esse é o titulo da noticias</strong>
-                <p>Esse paragráfo sera o conteúdo visível para o consumidor final</p>
-
-            </a>
+            {
+                posts.map(post => (
+                    <a href='#' key={post.slug}>
+                    <time>{post.updateAt}</time>
+                    <strong>{post.title}</strong>
+                    <p>{post.excerpt}</p>
+    
+                    </a>
+                ))
+            }
+           
+            
         </div>
     </main>
     </>
@@ -40,14 +48,26 @@ export default function Posts(){
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const response = await prismic.query([
+    const response = await prismic.query<any>([
         Prismic.predicates.at('document.type', 'post')
     ],{
         fetch:['post.title', 'post.content'],
         pageSize: 100,
     })
-    
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updateAt: new Date(post.last_publication_date).toLocaleDateString('pt-br',{
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
     return {
-        props: {}
+        props: { posts}
     }
 }
